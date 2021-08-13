6fcca4fded8ba38419121209eb68362f228b8ffe
Thomas Smith <thsm@umich.edu>
2021-08-12 22:27:17 -0400

    Add comments to main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,11 @@
 mod arch;
 mod panic;
 
+// An address that outputs to UART serial output when written to.
 static UART_MMIO_ADDR: i64 = 0x3F201000;
 
+// This is the Rust entry point for the OS. It should not halt except when a
+// fatal error occurs (i.e. the OS panics).
 pub fn main() -> ! {
     let string = "Hello, world!\n";
     for ch in string.chars() {
