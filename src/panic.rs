ea0aa3b886487ae7f42a0a43f1d42271802a6cb2
Thomas Smith <thsm@umich.edu>
2021-08-18 23:57:43 -0400

    Add comments to panic.rs

--- src/panic.rs
+++ src/panic.rs
@@ -1,5 +1,7 @@
 use crate::arch::asm::wait_forever;
 
+// The function to be called when a panic occurs. Right now, this halts the
+// processor (stack unwinding and similar features are not yet implemented).
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
     wait_forever()
