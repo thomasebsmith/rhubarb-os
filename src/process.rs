8fb86b469e267738c8933604dc32a69e054075d5
Thomas Smith <thsm@umich.edu>
2021-09-08 23:58:38 -0400

    Add documentation to process.rs

--- src/process.rs
+++ src/process.rs
@@ -2,5 +2,8 @@ type ProcessId = u64;
 
 struct Process {
     id: ProcessId,
+
+    // runner_id is None if this is the root process (so this process is not
+    //  controlled by any other process).
     runner_id: Option<ProcessId>,
 }
