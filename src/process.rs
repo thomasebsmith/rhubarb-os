31193eb3c734401032a3badbf318b4a80a453a55
Thomas Smith <thsm@umich.edu>
2021-09-09 23:53:44 -0400

    Explain process runners in more detail

--- src/process.rs
+++ src/process.rs
@@ -3,7 +3,10 @@ type ProcessId = u64;
 struct Process {
     id: ProcessId,
 
-    // runner_id is None if this is the root process (so this process is not
-    //  controlled by any other process).
+    // runner_id is None if this is the root process. Otherwise, runner_id the
+    //  ID of the process that handles all system calls for this process. This
+    //  allows one process to hook into another process (if given appropriate
+    //  permissions). For example, the runner can create fake hardware events
+    //  that appear to come from the user.
     runner_id: Option<ProcessId>,
 }
