b1f42b8179a086f80cd6e20a905037efd243783a
Thomas Smith <42979377+thomasebsmith@users.noreply.github.com>
2021-09-19 23:52:38 -0400

    Document alloc.rs

--- src/alloc.rs
+++ src/alloc.rs
@@ -1,5 +1,7 @@
+// Only 64-bit architectures are currently supported.
 pub u64 address_bits = 64;
 
+// TODO: Verify that this is correct.
 pub u64 page_size = 4 * 1024;
 
 pub u128 available_pages = 0; // TODO
