b373eb7f079cca340e079e6ad004d34afa3e6eb7
Thomas Smith <thsm@umich.edu>
2021-09-21 23:47:40 -0400

    Expand on available pages TODO

--- src/alloc.rs
+++ src/alloc.rs
@@ -4,4 +4,6 @@ pub u64 address_bits = 64;
 // TODO: Verify that this is correct.
 pub u64 page_size = 4 * 1024;
 
+// TODO: This will need to be more complicated - the actual set of available
+//  pages will need to be tracked.
 pub u128 available_pages = 0; // TODO
