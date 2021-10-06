2a9fdb6ad3498ddd2896485c35568a94822ff731
Thomas Smith <thsm@umich.edu>
2021-10-05 23:53:48 -0400

    Add megapage idea description to alloc.rs

--- src/alloc.rs
+++ src/alloc.rs
@@ -10,4 +10,9 @@ pub u64 page_size = 4 * 1024;
 
 // TODO: This will need to be more complicated - the actual set of available
 //  pages will need to be tracked.
+//  One way to do this would be to divide all physmem into "megapages" (of size
+//  ~16MB). The availability of megapages is tracked using the first 1+ physical
+//  pages. Within each megapage, the first physical page would track
+//  availability of regular pages.
+
 pub u128 available_pages = 0; // TODO
