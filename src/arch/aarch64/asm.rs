9197b5cbb2aad36aff15426aa431eff13b2d9565
Thomas Smith <thsm@umich.edu>
2021-10-26 23:54:57 -0400

    Convert comments to doc comments in asm.rs

--- src/arch/aarch64/asm.rs
+++ src/arch/aarch64/asm.rs
@@ -2,8 +2,8 @@
 // ---
 // Exposes assembly-level, processor-specific functionality using a generic API.
 
-// Waits forever (in an efficient manner). This does not busy-wait; rather,
-// it repeatedly waits for interrupts or events.
+/// Waits forever (in an efficient manner). This does not busy-wait; rather,
+/// it repeatedly waits for interrupts or events.
 pub fn wait_forever() -> ! {
     loop {
         unsafe {
