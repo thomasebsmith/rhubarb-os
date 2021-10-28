b50957074814eac89d6f7e06db6c7b154e8622af
Thomas Smith <42979377+thomasebsmith@users.noreply.github.com>
2021-10-27 23:52:50 -0400

    Explain supported architectures in arch.rs

--- src/arch.rs
+++ src/arch.rs
@@ -2,6 +2,9 @@
 // ---
 // Imports architecture-specific code (e.g. boot assembly).
 
+// Currently, only 64-bit ARM systems (aarch64) are supported.
+
+// aarch64
 #[cfg(target_arch = "aarch64")]
 mod aarch64;
 
