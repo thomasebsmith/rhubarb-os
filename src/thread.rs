dc20548de92754efa7c29a3aea16e50a019fd615
Thomas Smith <thsm@umich.edu>
2021-11-01 23:55:26 -0400

    Make Thread members public

--- src/thread.rs
+++ src/thread.rs
@@ -15,8 +15,8 @@ pub struct Thread {
     // Thread IDs are guaranteed to be unique among all running threads *within
     //  a single process* but may be reused after thread exits and within
     //  different processes.
-    id: ThreadId,
+    pub id: ThreadId,
 
     // The ID of the process that is running this thread.
-    parent_process_id: ProcessId,
+    pub parent_process_id: ProcessId,
 }
