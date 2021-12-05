c3fed2b0a3b58cdd7f1d6f24635a7b1f97fa5258
Thomas Smith <thsm@umich.edu>
2021-12-04 23:53:50 -0500

    Add SpinLock members

--- src/synchronization.rs
+++ src/synchronization.rs
@@ -1,14 +1,17 @@
+use std::sync::atomic::{AtomicU64, AtomicBool};
+
 pub struct SpinLock<T: ?Sized> {
-    // TODO
+    AtomicU64 holder_thread_id;
+    AtomicBool held;
 }
 
 impl<T> SpinLock<T> {
     pub fn new(t: T) -> SpinLock<T> {
-        // TODO
+        return SpinLock{0, 0, false};
     }
 
     pub fn lock(&self) -> MutexGuard<'_, T> {
-        // TODO
+        // TODO: while !self.try_lock() {}
     }
 
     pub fn try_lock(&self) -> Option<MutexGuard<'_, T>> {
