555a28b9f0a7d7c07855923b6a1019043d4db47a
Thomas Smith <thsm@umich.edu>
2021-12-19 23:54:57 -0500

    Add use statements for asm macros

--- src/arch/aarch64/asm.rs
+++ src/arch/aarch64/asm.rs
@@ -2,6 +2,8 @@
 // ---
 // Exposes assembly-level, processor-specific functionality using a generic API.
 
+use core::arch::asm;
+
 /// Waits forever (in an efficient manner). This does not busy-wait; rather,
 /// it repeatedly waits for interrupts or events.
 pub fn wait_forever() -> ! {
