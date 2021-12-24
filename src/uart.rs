e0284417526fdd78fdc6553a58117630b2edd638
Thomas Smith <thsm@umich.edu>
2021-12-23 23:41:49 -0500

    Make entire print!s and println!s atomic

--- src/uart.rs
+++ src/uart.rs
@@ -3,15 +3,15 @@
 // Code for printing to UART serial output.
 
 use core::fmt;
-use crate::sync::SpinLock;
+use crate::sync::{MutexGuard, SpinLock};
 
 // A mutex protecting an address that outputs to UART serial output when
 // written to.
 static UART_MMIO_ADDR_LOCK: SpinLock<i64> = SpinLock::new(0x3F201000 as i64);
 
-// Writes a single character to addr. Not thread-safe.
-unsafe fn print_char(ch: char, addr: i64) {
-    core::ptr::write_volatile(addr as *mut u8, ch as u8);
+// Writes a single character to addr.
+unsafe fn print_char(ch: char, guard: &MutexGuard<'_, i64>) {
+    core::ptr::write_volatile(**guard as *mut u8, ch as u8);
 }
 
 /// Prints a string to UART using MMIO.
@@ -19,17 +19,18 @@ unsafe fn print_char(ch: char, addr: i64) {
 /// # Arguments
 ///
 /// * `string` - The string to print
-pub unsafe fn print_str(string: &str) {
-    let guard = UART_MMIO_ADDR_LOCK.lock();
+unsafe fn print_str(string: &str, guard: &MutexGuard<'_, i64>) {
     for ch in string.chars() {
-        print_char(ch, *guard)
+        print_char(ch, guard)
     }
 }
 
 /// A struct that conforms to fmt::write and writes to UART via MMIO.
-struct Writer {}
+struct Writer<'a> {
+    guard: &'a MutexGuard<'a, i64>,
+}
 
-impl fmt::Write for Writer {
+impl fmt::Write for Writer<'_> {
     /// Writes a string to UART.
     ///
     /// # Arguments:
@@ -38,9 +39,10 @@ impl fmt::Write for Writer {
     ///
     /// # Return value:
     /// Whether the write succeeded
+
     fn write_str(&mut self, string: &str) -> fmt::Result {
         unsafe {
-            print_str(string)
+            print_str(string, self.guard)
         }
         Ok(())
     }
@@ -53,7 +55,8 @@ pub fn _print(args: fmt::Arguments) {
     // It's OK to unwrap the result of write_fmt here.
     // If formatting fails, unwrap will panic, which is the desired behavior of
     //  print! and println!.
-    Writer{}.write_fmt(args).unwrap();
+    let guard = UART_MMIO_ADDR_LOCK.lock();
+    Writer{guard: &guard}.write_fmt(args).unwrap();
 }
 
 /// Prints formatted argument(s) to UART using MMIO.
