945f4df02cf3f529778a0b15a1ef1b7786f020f1
Thomas Smith <thsm@umich.edu>
2022-01-12 23:50:02 -0500

    Use consistent terminology in panic() docs

--- src/panic.rs
+++ src/panic.rs
@@ -1,18 +1,18 @@
 // panic
 // ---
-// Code for handling kernel crashes. This shouldn't run much. :)
+// Code for handling kernel panics. This shouldn't run much. :)
 
 use crate::arch::asm::wait_forever;
 use crate::println;
 
-/// Ends the OS (causing it to crash) after printing crash information
+/// Ends the OS (causing it to crash) after printing panic information
 ///
 /// Currently halts the processor (stack unwinding and similar features are not
 /// yet implemented).
 ///
 /// # Arguments
 ///
-/// * `info` - Information about the crash
+/// * `info` - Information about the panic
 #[panic_handler]
 pub fn panic(info: &core::panic::PanicInfo) -> ! {
     println!("{}", info);
