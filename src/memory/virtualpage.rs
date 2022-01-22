1ab61027f1967d63c314f1d3e47d4c19f8d473c1
Thomas Smith <thsm@umich.edu>
2022-01-21 23:56:04 -0500

    Explain VirtualPageLocation enum cases

--- src/memory/virtualpage.rs
+++ src/memory/virtualpage.rs
@@ -9,7 +9,10 @@ pub struct PhysicalPageNumber(pub u64);
 pub struct VirtualPageNumber(pub u64);
 
 pub enum VirtualPageLocation {
+    // Used for pages stored in swap space
     Evicted { diskBlock: DiskBlock },
+
+    // Used for pages stored in memory
     Resident { physicalPageNumber: PhysicalPageNumber },
 }
 
