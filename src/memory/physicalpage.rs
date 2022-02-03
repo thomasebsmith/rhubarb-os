75d8da0f85bb0f7e3d4dd259b94b5c865f840bcc
Thomas Smith <thsm@umich.edu>
2022-02-02 23:51:06 -0500

    Add initial implementation of pointer_to function

--- src/memory/physicalpage.rs
+++ src/memory/physicalpage.rs
@@ -5,6 +5,8 @@
 pub struct DiskBlock(pub u64);
 pub struct PhysicalPageNumber(pub u64);
 
-pub unsafe fn pointer_to(ppn: PhysicalPageNumber) -> *u8 {
-    // TODO
+pub const usize PAGE_SIZE = 4 * 1024;
+
+pub unsafe fn pointer_to(ppn: PhysicalPageNumber) -> *mut u8 {
+    return (ppn * PAGE_SIZE) as *mut u8;
 }
