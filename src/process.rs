6ca31b012543b8c3fb5f59414e442a8d04108d4d
Thomas Smith <42979377+thomasebsmith@users.noreply.github.com>
2023-05-04 23:58:15 -0400

    Fix typo in process.rs comment

--- src/process.rs
+++ src/process.rs
@@ -20,11 +20,11 @@ pub struct Process {
     //  processes. However, process IDs of exited processes may be reused.
     pub id: ProcessId,
 
-    // runner_id is None if this is the root process. Otherwise, runner_id the
-    //  ID of the process that handles all system calls for this process. This
-    //  allows one process to hook into another process (if given appropriate
-    //  permissions). For example, the runner can create fake hardware events
-    //  that appear to come from the user.
+    // runner_id is None if this is the root process. Otherwise, runner_id is
+    //  the ID of the process that handles all system calls for this process.
+    //  This allows one process to hook into another process (if given
+    //  appropriate permissions). For example, the runner can create fake
+    //  hardware events that appear to come from the user.
     pub runner_id: Option<ProcessId>,
 }
 
